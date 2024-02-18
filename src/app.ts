220eb096d880750253242402b1567cb89ecf3229
martin <MartinChrisPC@hotmail.com>
2024-02-17 23:25:21 -0500

    ADD app config

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import { config } from 'dotenv'
+import { router } from './routes'
+import { AppDataSouce } from './config/datasource'
+import 'reflect-metadata'
+
+config()
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+app.disable('x-powered-by')
+
+AppDataSouce.initialize()
+	.then(() => console.log('Database Conected'))
+	.catch(console.error)
+
+app.use('/api/v1', router)
+
+app.listen(PORT, () => {
+	console.log(`Api funcionando correctamente en http://localhost:${PORT}`)
+})

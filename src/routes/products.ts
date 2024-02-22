2cbfb46227d3f9996ba47056dbe29e9e82774d69
MartinPC <MartinChrisPC@hotmail.com>
2024-02-21 22:36:05 -0500

    UPDATE routes protected

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,8 +1,33 @@
+import { HandleSession } from '../middleware/Handlesession'
 import { ProductController } from '../controllers/products.controller'
 import { Router } from 'express'
+import { HandleRoles } from '../middleware/HandleRoles'
+import { multerOptions } from '../middleware/HandleMulter'
 export const router = Router()
 router.get('/', ProductController.findAll)
 router.get('/:id', ProductController.findById)
-router.post('/', ProductController.create)
-router.patch('/:id', ProductController.update)
-router.delete('/:id', ProductController.delete)
+router.post(
+	'/',
+	[
+		multerOptions.single('image'),
+		HandleRoles.isAdmin,
+		HandleRoles.isEmployee,
+		HandleSession.checkJwt
+	],
+	ProductController.create
+)
+router.patch(
+	'/:id',
+	[
+		multerOptions.single('image'),
+		HandleRoles.isAdmin,
+		HandleRoles.isEmployee,
+		HandleSession.checkJwt
+	],
+	ProductController.update
+)
+router.delete(
+	'/:id',
+	[HandleRoles.isAdmin, HandleRoles.isEmployee, HandleSession.checkJwt],
+	ProductController.delete
+)

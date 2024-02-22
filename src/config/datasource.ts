992743f1b04f58eaf0dcd6796bd958277cf6dc75
MartinPC <MartinChrisPC@hotmail.com>
2024-02-21 22:36:36 -0500

    UPDATE env variables

--- src/config/datasource.ts
+++ src/config/datasource.ts
@@ -1,14 +1,17 @@
 import { User } from '../entities/users.entity'
 import { Product } from '../entities/product.entity'
 import { DataSource } from 'typeorm'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 export const AppDataSouce = new DataSource({
 	type: 'mysql',
-	host: 'localhost',
-	port: 3306,
-	username: 'root',
-	password: '',
-	database: 'puntoventadb',
+	host: process.env.DB_HOST,
+	port: Number(process.env.DB_PORT),
+	username: process.env.DB_USERNAME,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
 	entities: [Product, User],
 	synchronize: true
 })

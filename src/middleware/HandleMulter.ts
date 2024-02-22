e416cccfb639b4b5bec47618c5954475b1b5047a
MartinPC <MartinChrisPC@hotmail.com>
2024-02-21 22:37:12 -0500

    ADD multer config

---
+++ src/middleware/HandleMulter.ts
@@ -0,0 +1,30 @@
+import multer, { diskStorage } from 'multer'
+import { existsSync, mkdirSync } from 'fs'
+import { extname } from 'path'
+import { v4 as uuid } from 'uuid'
+
+export const multerOptions = multer({
+	limits: {
+		fileSize: 50000000
+	},
+	fileFilter: (req: any, file: Express.Multer.File, cb: any) => {
+		if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
+			cb(null, true)
+		} else {
+			cb('Invalid  image type', false)
+		}
+	},
+	storage: diskStorage({
+		destination: (req: any, file: Express.Multer.File, cb: any) => {
+			const uploadPath = './public/images'
+			if (!existsSync(uploadPath)) {
+				mkdirSync(uploadPath)
+			} else {
+				cb(null, uploadPath)
+			}
+		},
+		filename: (req: any, file: Express.Multer.File, cb: any) => {
+			cb(null, `${uuid()}${extname(file.originalname)}`)
+		}
+	})
+})
